f579a6dbeef5a0315f712de59d6ccec179a6d5f9
xwj <609599079@qq.com>
2016-03-13 20:58:18 +0800

    这次收到了把

---
+++ src/com/x/x/ZhangYunFei.java
@@ -0,0 +1,12 @@
+package com.x.x;
+
+import java.io.Serializable;
+
+public class ZhangYunFei implements Serializable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
